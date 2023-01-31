367bf78fde3d2063576783a731fa8c523c1ad770
Braeden <braedenc@email.unc.edu>
2023-01-30 23:13:08 -0500

    Progress on EX01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,10 +2,10 @@
 __author__ = "730405020"
 
 word: str = input("Enter a 5-character word: ")
-if len(word) < 5:
-   exit("Error: Word must contain 5 characters")
-
-letter: str = input("Enter a single character: ")
+if len(word) < 5: 
+    exit("Error: Word must contain 5 characters")
+    
+letter: str = input("Enter a 5 character word: ")
 if len(letter) != 1:
     exit("Error: Character must be a single character.")
 
@@ -19,15 +19,19 @@ if letter == word[1]:
     print(letter + " found at index 1")
     number_instances = number_instances + 1
 if letter == word[2]:
-    print(letter + " found at index 2")
+    print (letter + " found at index 2")
     number_instances = number_instances + 1
 if letter == word[3]:
-    print(letter + " found at index 3")
+    print (letter + " found at index 3")
     number_instances = number_instances + 1
 if letter == word[4]:
-    print(letter + " found at index 4")
+    print (letter + " found at index 4")
     number_instances = number_instances + 1
 if number_instances == 0:
     print("No instances of " + letter + " found in " + word)
+if number_instances == 1:
+    print("1 instance of " + letter + " found in " + word)
 else:
     print(str(number_instances) + " instances of " + letter + " found in " + word)
+
+        
