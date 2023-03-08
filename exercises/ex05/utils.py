4cb5e5b1bc79b5cdff28c48fe52fbec592cfe470
Braeden <braedenc@email.unc.edu>
2023-03-08 02:07:35 -0500

    progress on ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,16 +1,18 @@
 """EX05 - list Utility Functions."""
 __author__ = "730405020"
 
+
 def only_evens(numbers: list[int]) -> list[int]:
-    """returns list of even integers/numbers from input parameters."""
+    """Returns list of even integers/numbers from input parameters."""
     evens = []
     for n in numbers:
         if n % 2 == 0:
             evens.append(n)
     return evens
 
+
 def concat(list_1: list[int], list_2: list[int]) -> list[int]:
-    """returns a list containing all elements of 1st and 2nd lists."""
+    """Returns a list containing all elements of 1st and 2nd lists."""
     final_list = []
     for n in list_1:
         final_list.append(n)
@@ -18,11 +20,12 @@ def concat(list_1: list[int], list_2: list[int]) -> list[int]:
         final_list.append(n)
     return final_list
 
+
 def sub(a_list: list[int], start_idx: int, end_idx: int) -> list[int]:
-    """returns a subset of a given list between specified start and end indexes."""
+    """Returns a subset of a given list between specified start and end indexes."""
     if start_idx < 0:
         start_idx = 0 
-    if end_idx > len(a_list):
-        end_idx = len(a_list) - 1
+    if end_idx > len(a_list) - 1:
+        end_idx == len(a_list) - 1
     sub_list = a_list[start_idx: end_idx]
     return sub_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -5,54 +5,62 @@ from exercises.ex05.utils import only_evens, sub, concat
 
 
 def test_empty() -> None:
-    """edge case: will function give an empty list if input is all odd?"""
+    """Edge case: will function give an empty list if input is all odd?"""
     test_list: list[int] = [3, 5, 7]
     assert only_evens(test_list) == []
 
+
 def test_evens_1() -> None:
-    """use case: will function pick even numbers out of list?"""
+    """Use case: will function pick even numbers out of list?"""
     test_list: list[int] = [1, 4, 7]
     assert only_evens(test_list) == [4]
 
+
 def test_evens_2() -> None:
-    """use case: will function pick even numbers out of list?"""
+    """Use case: will function pick even numbers out of list?"""
     test_list: list[int] = [2, 3, 6]
     assert only_evens(test_list) == [2, 6]
 
+
 def test_empty_2() -> None:
-    """edge case: will function add empty list to filled list?"""
+    """Edge case: will function add empty list to filled list?"""
     test_list_1: list[int] = [3, 5, 7]
     test_list_2: list[int] = []
     assert concat(test_list_1, test_list_2) == [3, 5, 7]
 
+
 def test_add_1() -> None:
-    """use case: will function combine both lists together?"""
+    """Use case: will function combine both lists together?"""
     test_list_1: list[int] = [3, 5, 7]
     test_list_2: list[int] = [2, 3, 6]
     assert concat(test_list_1, test_list_2) == [3, 5, 7, 2, 3, 6]
 
+
 def test_add_2() -> None:
-    """edge case: will function combine both lists together?"""
+    """Use case: will function combine both lists together?"""
     test_list_1: list[int] = [1, 5, 7]
     test_list_2: list[int] = [2, 6, 8]
     assert concat(test_list_1, test_list_2) == [1, 5, 7, 2, 6, 8]
 
+
 def test_negative() -> None:
-    """edge case: will the function start from the beginnig of the list if start index is negative?"""
+    """Edge case: will the function start from the beginnig of the list if start index is negative?"""
     a_list: list[int] = [1, 2, 3, 4]
     start_idx = -1
     end_idx = 3
     assert sub(a_list, start_idx, end_idx) == [1, 2, 3]
 
+
 def test_positive_1() -> None:
-    """use case: will the function make the subset in the proper range?"""
+    """Use case: will the function make the subset in the proper range?"""
     a_list: list[int] = [1, 2, 3, 4]
     start_idx = 1
     end_idx = 3
     assert sub(a_list, start_idx, end_idx) == [2, 3]
+    
 
 def test_positive_2() -> None:
-    """use case: will the function make the subset in the proper range?"""
+    """Use case: will the function make the subset in the proper range?"""
     a_list: list[int] = [1, 2, 3, 4]
     start_idx = 2
     end_idx = 4
