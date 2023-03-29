b85879cfbe3701249b3cf412b5b17059437a8dd2
Braeden <braedenc@email.unc.edu>
2023-03-29 13:32:22 -0400

    Progress on Exercise 7

---
+++ exercises/ex07/dictionary.py
@@ -0,0 +1,49 @@
+"""EX07 - Dictionary Functions."""
+__author__ = "730405020"
+
+
+def invert(dictionary: dict[str, str]) -> dict[str, str]:
+    """Inverts initial dictionary."""
+    inverted: dict[str, str] = {}
+    for key in dictionary:
+        value = dictionary[key]
+        if value in inverted:
+            raise KeyError("KeyError")
+        else:
+            inverted[value] = key
+    return inverted
+    
+
+def favorite_color(dictionary: dict[str, str]) -> str:
+    """Determines and returns favorite color from dictionary."""
+    new_dict: dict[str, str] = {}
+    for person in dictionary:
+        color = dictionary[person]
+        if color in new_dict:
+            new_dict[color] += 1
+        else: 
+            new_dict[color] = 1
+    max_color: int = 0
+    return_color: str = ""
+    for color in new_dict:
+        if new_dict[color] > max_color:
+            max_color = new_dict[color]
+            return_color = color
+    return return_color
+
+
+def count(list: list[str]) -> dict[str, int]:
+    """Returns a dictionary showing how many times an item was in a list."""
+    dictionary: dict[str, int] = {}
+    for item in list:
+        if item in dictionary:
+            dictionary[item] += 1
+        else:
+            dictionary[item] = 1
+    return dictionary 
+
+        
+
+
+    
+
---
+++ exercises/ex07/dictionary_test.py
@@ -0,0 +1,61 @@
+"""EX07 - Unit Tests for Dictionary Functions."""
+__author__ = "730405020"
+
+
+from dictionary import invert, favorite_color, count
+import pytest
+
+
+def test_error() -> None:
+    with pytest.raises(KeyError):
+        """Edge case: will function return 'KeyError' if there are two same values in dictionary?"""
+        test_dict: dict[str, str] = {'kris': 'jordan', 'michael': 'jordan'}
+        invert(test_dict)
+
+
+def test_invert_1() -> None:
+    """Use case: will fucntion invert dictionary of letters?"""
+    test_dict: dict[str, str] = {'a': 'z', 'b': 'y', 'c': 'x'}
+    assert invert(test_dict) == {'z': 'a', 'y': 'b', 'x': 'c'}
+
+
+def test_invert_2() -> None:
+    """Use case: will fucntion invert dictionary?"""
+    test_dict: dict[str, str] = {'apple': 'cat'}
+    assert invert(test_dict) == {'cat': 'apple'}
+
+
+def test_tie() -> None:
+    """Edge case: will function return the color that appearred first in the dictionary?"""
+    test_dict: dict[str, str] = {'Marc': 'yellow', 'Ezri': 'blue', 'Kris': 'blue', 'Braeden': 'yellow'}
+    assert favorite_color(test_dict) == "yellow"
+
+
+def test_favorite_color_1() -> None:
+    """Use case: will function return the color that appears most in dictionary?"""
+    test_dict: dict[str, str] = {'Marc': 'yellow', 'Ezri': 'blue', 'Kris': 'blue'}
+    assert favorite_color(test_dict) == "blue"
+
+
+def test_favorite_color_2() -> None:
+    """Use case: will function return the color that appears most in dictionary?"""
+    test_dict: dict[str, str] = {'Marc': 'yellow', 'Ezri': 'yellow', 'Kris': 'blue'}
+    assert favorite_color(test_dict) == "yellow"
+
+
+def test_empty() -> None:
+    """Edge case: will the function return an empty dictionary if list is empty?"""
+    test_list: list[str] = ()
+    assert count(test_list) == {}
+
+
+def test_count_1() -> None:
+    """Use case: will the function return an dictionary showing how many times an item was in the list?"""
+    test_list: list[str] = ('yellow', 'yellow', 'blue')
+    assert count(test_list) == {'yellow': 2, 'blue': 1}
+
+
+def test_count_2() -> None:
+    """Use case: will the function return an dictionary showing how many times an item was in the list?"""
+    test_list: list[str] = ('yellow', 'yellow', 'blue', 'blue')
+    assert count(test_list) == {'yellow': 2, 'blue': 2}
